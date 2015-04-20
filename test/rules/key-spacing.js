4e0848ed11e114aaac812a999715df1ce977c232
Nate-Wilkins <nwilkins2012@gmail.com>
2015-04-20 16:20:50 -0400

    Fix option defaults for 'key-spacing'

---
+++ test/rules/key-spacing.js
@@ -0,0 +1,75 @@
+var testRule = require('./../test-rule');
+
+var multiline = require('multiline');
+
+testRule('key-spacing', [
+  {
+    msg: 'rule off',
+    rules: { 'key-spacing': 0 },
+    input: multiline.stripIndent(function () {/*
+      var obj = {
+        foo : 42
+      };
+    */})
+  },
+  {
+    msg: 'rule on, default beforeColon false, afterColon true',
+    rules: { 'key-spacing': [2] },
+    input: multiline.stripIndent(function () {/*
+      var obj = { foo : 42 };
+    */}),
+    formatted: multiline.stripIndent(function () {/*
+      var obj = {
+        foo: 42
+      };
+    */})
+  },
+  {
+    msg: 'rule on, beforeColon true, afterColon false',
+    rules: { 'key-spacing': [2, { beforeColon: true, afterColon: false }] },
+    input: multiline.stripIndent(function () {/*
+      var obj = { foo: 42 };
+    */}),
+    formatted: multiline.stripIndent(function () {/*
+      var obj = {
+        foo :42
+      };
+    */})
+  },
+  {
+    msg: 'rule on, beforeColon true, afteColon unspecified',
+    rules: { 'key-spacing': [2, { beforeColon: true }] },
+    input: multiline.stripIndent(function () {/*
+      var obj = { foo: 42 };
+    */}),
+    formatted: multiline.stripIndent(function () {/*
+      var obj = {
+        foo : 42
+      };
+    */})
+  },
+  {
+    msg: 'rule on, beforeColon false, afteColon unspecified',
+    rules: { 'key-spacing': [2, { beforeColon: false }] },
+    input: multiline.stripIndent(function () {/*
+      var obj = { foo : 42 };
+    */}),
+    formatted: multiline.stripIndent(function () {/*
+      var obj = {
+        foo: 42
+      };
+    */})
+  },
+  {
+    msg: 'rule on, afterColon true, beforeColon unspecified',
+    rules: { 'key-spacing': [2, { afterColon: true }]},
+    input: multiline.stripIndent(function () {/*
+      var obj = { foo : 42 };
+    */}),
+    formatted: multiline.stripIndent(function () {/*
+      var obj = {
+        foo: 42
+      };
+    */})
+  }
+]);
